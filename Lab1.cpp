793aeeff9a8ed0911662b812f8155b5ea0e50cda
Raymond Bedry <46666166+raybeezzz@users.noreply.github.com>
2019-02-10 21:40:51 -0800

    Add files via upload
    Commenting and formatting

--- Lab1.cpp
+++ Lab1.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <iomanip>
 
+//
 string filename;
 vector<JOINT> joint_vec;
 vector<MEMBER> mem_vec;
@@ -90,13 +91,16 @@ TRUSS_SIZES getTrussSizes()
 
 }
 
-void getTrussData(TRUSS_SIZES ts, vector<JOINT> Jf, vector<MEMBER> Mf, vector<FORCE> Ef, vector<FORCE> Rf, string strUnits) {
+void getTrussData(TRUSS_SIZES ts, vector<JOINT> Jf, vector<MEMBER> Mf, vector<FORCE> Ef,
+	vector<FORCE> Rf, string strUnits)
+{
+	ifstream get_truss(filename); //create file stream from user input text file name
+	string line; //string creation for pushing data into from text file
+	int joint, index; //variables to allocate correct forces to joints in vectors
+	int header_pos = 0; //initalize header position variable to 0
+	char direction; //variable to take in reaction force direction
 
-	ifstream get_truss(filename);
-	string line;
-	int joint, index;
-	int header_pos = 0;
-	char direction;
+	//vector creation for storing headers:
 	vector<string> headers;
 	headers.push_back(JOINT_COORDINATE_HEADER);
 	headers.push_back(MEMBER_CONNECTIVITY_HEADER);
@@ -104,44 +108,38 @@ void getTrussData(TRUSS_SIZES ts, vector<JOINT> Jf, vector<MEMBER> Mf, vector<FO
 	headers.push_back(EXTERNAL_FORCES_HEADER);
 	headers.push_back(FORCE_UNITS_HEADER);
 
-	double magnitude;
-	int connection1, connection2;
-
-
+	//While loop for going through all headers in text file
 	while (header_pos < headers.size())
 	{
 		//stringstream temp(line);
-		getline(get_truss, line);
-		if (line.empty())
+		getline(get_truss, line);//get next line of data and store it into line
+		if (line.empty())//while next line is not empty
 		{
 			get_truss.ignore(256, '\n');
 		}
-		switch (header_pos)
+		switch (header_pos) //switch structure whith cases coresponding to header vector position
 		{
 		case 0:
-			//get_truss.ignore(256, '\n');
-			getline(get_truss, line);
-			while (!line.empty())
+			getline(get_truss, line);//get next line of data and store it into line
+			while (!line.empty())//while next line is not empty
 			{
 				stringstream temp(line);
 				JOINT new_joint;
 				temp >> joint >> new_joint.p[0] >> new_joint.p[1];
-				getline(get_truss, line);
+				getline(get_truss, line);//get next line of data and store it into line
 				Jf.push_back(new_joint);
 			}
 			joint_vec = Jf;
 			header_pos++;
 			break;
 		case 1:
-			getline(get_truss, line);
-			while (!line.empty())
+			getline(get_truss, line);//get next line of data and store it into line
+			while (!line.empty())//while next line is not empty
 			{
 				MEMBER new_member;
-
 				stringstream temp1(line);
 				temp1 >> joint >> new_member.j[0] >> new_member.j[1];
-				getline(get_truss, line);
-
+				getline(get_truss, line);//get next line of data and store it into line
 				Mf.push_back(new_member);
 			}
 			mem_vec = Mf;
@@ -149,63 +147,57 @@ void getTrussData(TRUSS_SIZES ts, vector<JOINT> Jf, vector<MEMBER> Mf, vector<FO
 			break;
 		case 2:
 			getline(get_truss, line);
-			while (!line.empty())
+			while (!line.empty())//while next line is not empty
 			{
 				FORCE rxn;
 				char dir;
 				stringstream temp2(line);
 				temp2 >> index >> rxn.j >> dir;
-				if (dir == 'X') {
+				if (dir == 'X')
+				{
 					rxn.idir = 0;
 				}
-				else {
+				else 
+				{
 					rxn.idir = 1;
 				}
-				getline(get_truss, line);
-
+				getline(get_truss, line);//get next line of data and store it into line
 				Rf.push_back(rxn);
 			}
 			rxn_vec = Rf;
 			header_pos++;
 			break;
 		case 3:
-			getline(get_truss, line);
-			while (!line.empty())
+			getline(get_truss, line);//get next line of data and store it into line
+			//While loop for external forces vector loading:
+			while (!line.empty()) //while next line is not empty
 			{
-				FORCE ext;
-				char dir;
-				stringstream temp3(line);
-
-				temp3 >> index >> ext.j >> ext.F >> dir;
-
+				FORCE ext; //create instane of Force struct in lab1.h
+				char dir; //variable to store direction of force
+				stringstream temp3(line); //creation of a stream for this sections data
+				temp3 >> index >> ext.j >> ext.F >> dir; //loading of data into appropriate variables
 				if (dir == 'X') {
 					ext.idir = 0;
 				}
 				else {
 					ext.idir = 1;
 				}
-
-				getline(get_truss, line);
-
+				getline(get_truss, line);//get next line of data and store it into line
 				Ef.push_back(ext);
-			}
+			}//end while for external forces vector loading
 			ext_vec = Ef;
 			header_pos++;
 			break;
 		case 4:
-			getline(get_truss, line);
-
+			getline(get_truss, line);//get next line of data and store it into line
 			stringstream temp4(line);
-			temp4 >> units;
-			getline(get_truss, line);
-			header_pos++;
-
-			//getline(get_truss, line);
-
+			temp4 >> units; //store units string into variable
+			getline(get_truss, line); //get next line of data and store it into line
+			header_pos++; //increment header position
 			break;
-		}
-	}
-}
+		}//end switch
+	}//end while
+}//end getTrussData
 
 bool buildSystem()
 {
@@ -218,20 +210,20 @@ bool buildSystem()
 	int start_joint, end_joint;
 	N = (joint_vec.size() * 2);
 	E = new double[N];
-
 	M = new double *[N];
 	Minv = new double *[N];
-
 	indx = new int[N];
 	row = new double[N];
 	col = new double[N];
 
+	//creation of columns for every row in Matrix and Matrix Inverse:
 	for (int i = 0; i < N; i++)
 	{
 		M[i] = new double[N];
 		Minv[i] = new double[N];
 	}
 
+	//Building of Matrix M:
 	while (rows < N)
 	{
 
@@ -255,9 +247,8 @@ bool buildSystem()
 				//{
 				if ((current_joint == start_joint) || (current_joint == end_joint))
 				{
-
-
-					length = sqrt((pow((joint_vec[end_joint].p[0] - joint_vec[start_joint].p[0]), 2.0) + pow((joint_vec[end_joint].p[1] - joint_vec[start_joint].p[1]), 2.0)));
+					length = sqrt((pow((joint_vec[end_joint].p[0] - joint_vec[start_joint].p[0]), 2.0) + 
+						pow((joint_vec[end_joint].p[1] - joint_vec[start_joint].p[1]), 2.0)));
 					M[rows][columns] = (joint_vec[end_joint].p[X_Y] - joint_vec[start_joint].p[X_Y]) / length;
 
 				}
@@ -272,15 +263,14 @@ bool buildSystem()
 
 			for (columns; columns < mem_vec.size() + rxn_vec.size(); columns++)
 			{
-				
-					if ((current_joint == rxn_vec[columns - mem_vec.size()].j) && (rxn_vec[columns - mem_vec.size()].idir == X_Y)) { //changed to - mem_vec.size() instead of rxn_vec.size()
+					if ((current_joint == rxn_vec[columns - mem_vec.size()].j) && 
+						(rxn_vec[columns - mem_vec.size()].idir == X_Y)) 
+					{ 
 						M[rows][columns] = 1;
 					}
 					else {
 						M[rows][columns] = 0;
 					}
-				
-				//cout << M[rows][columns] << endl;
 			}
 
 			if (counter < ext_vec.size())
@@ -289,7 +279,6 @@ bool buildSystem()
 				{
 					E[rows] = -ext_vec[counter].F;
 					counter++;
-
 				}
 				else
 				{
@@ -300,22 +289,14 @@ bool buildSystem()
 			{
 				E[rows] = 0;
 			}
-			//out << E[rows];
 
 			columns = 0;
 			rows++;
 		}//end x_y for
-
-
-	//cout << M[rows][columns] << endl;
-
-		current_joint++;
-
-	}//END WHILE
+		current_joint++; //increment current joint
+	}//end building of Matrix M
 	return 1;
-}
-
-
+}//end buildsystem function 
 
 /* Function to invert matrix A with the inverse stored in Ainv.*/
 void InverseNxN(double **A, int N, double **Ainv, int *indx)
@@ -472,39 +453,28 @@ void calculate_answer()
 int main() {
 	getTrussData(getTrussSizes(), joint_vec, mem_vec, ext_vec, rxn_vec, units);
 	buildSystem();
-
-
-	
 	string output_name;
-
 	cout << "What would you like to call output text file? (Exclude .txt)";
 	cin >> output_name;
 	output_name.append(".txt");
 	ofstream outfile(output_name);
-	
-	
 	if (!outfile)
 	{
 		std::cout << "oops, something went wrong! Please contact IT for support" << std::endl;
 		return 0;
 	}
-	//outfile.precision(2);
-	//outfile.setf(ios::fixed);
-	//outfile.setf(ios::showpoint);
-	//Print Matrix A
 	outfile << "Matrix M" << endl << "---------" << endl;
 	printf("Matrix M:\n");
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			
 			if (M[i][j] >= 0)
 			{
 				cout << "+";
 				outfile << "+";
 			}
-			printf("%.2lf%c", M[i][j], (j < N - 1 ? '\t' : '\n')); //help from online - need to cite
+			printf("%.2lf%c", M[i][j], (j < N - 1 ? '\t' : '\n'));//http://www.angelfire.com/oh4/psychfea/source.htm
 			outfile << fixed << M[i][j] << "\t";
 			
 			if (j >= (N - 1))
@@ -513,10 +483,11 @@ int main() {
 			}
 		}
 	}
+	InverseNxN(M, N, Minv, indx); //Creation of Inverse matrix for using to get final answer
+	calculate_answer(); //Call Calculate Answer function for Matrix Multiplication 
 
-	InverseNxN(M, N, Minv, indx); //Must be called after printing Matrix M b/c it manipulates Matrix M
-	calculate_answer();
-	printf("\n\nReaction Forces Answer:\n");
+	//Printing and Saving of Reaction and Member Forces to text file:
+	printf("\n\nReaction and Member Forces:\n");
 	int print_counter = 0;
 	outfile << endl << endl << endl << "Solution" << endl << " ---------" << endl;
 	for (int i = 0; i < N; i++)
@@ -543,7 +514,6 @@ int main() {
 		{
 			if (rxn_vec[print_counter].idir == 0) 
 			{
-
 				cout << "X-direction reaction on joint " << rxn_vec[print_counter].j << " =  ";
 				outfile << "X-direction reaction on joint " << rxn_vec[print_counter].j << " =  ";
 				if (Answer[i] >= 0)
@@ -561,7 +531,6 @@ int main() {
 			}
 			else
 			{
-			
 				cout << "Y-direction reaction on joint " << rxn_vec[print_counter].j << " =  ";
 				outfile << "Y-direction reaction on joint " << rxn_vec[print_counter].j << " =  ";
 				if (Answer[i] > 0)
@@ -579,7 +548,6 @@ int main() {
 			}
 			print_counter++;
 		}
-
 	}
 	outfile.close();
 }
